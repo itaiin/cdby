42a7f4eade0c9cc82852658cd4017595337894a8
Itai Incze <itai@SPEEDATA-LAPTOP-ITAI>
2023-02-13 14:53:54 +0200

    Alter behaviour for directories

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ struct Args {
 
     #[arg()]
     pattern: String,
+
+    #[arg(long, default_value = "false")]
+    dir_parent: bool,
 }
 
 fn to_cstr(s: &str) -> CString {
@@ -22,10 +25,18 @@ fn to_cstr(s: &str) -> CString {
 fn do_it(args: Args) {
     let re = Regex::new(args.pattern.as_str()).unwrap();
     for entry in WalkDir::new(args.root).into_iter().filter_map(|e| e.ok()) {
-        let path_str = entry.path().to_str().unwrap();
+        let path = entry.path();
+        let path_str = path.to_str().unwrap();
         if re.is_match(path_str) {
             println!("{}", path_str);
-            std::env::set_current_dir(entry.path().parent().unwrap()).unwrap();
+            let jump_to = if path.is_file() || path.is_symlink() || args.dir_parent {
+                path.parent().unwrap()
+            } else if path.is_dir() {
+                path
+            } else {
+                panic!("Unsupported file type");
+            };
+            std::env::set_current_dir(jump_to).unwrap();
             execv(to_cstr("/bin/bash").as_c_str(), &[to_cstr("bash")]).unwrap();
         }
     }
